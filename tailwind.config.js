d9999fe62c2759ff9cc51e2e65fbd331d51f2e13
DarKSoul Atik <anantoshahrear10@gmail.com>
2025-02-07 16:04:03 +0600

    banner done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+
+
+        animation: {
+          textFade: "textFade 2s ease-in-out infinite",
+        },
+        keyframes: {
+          textFade: {
+            "0%, 100%": { color: "black" },
+            "50%": { color: "yellow" },
+          },
+        },    
+      },
     },
     plugins: [],
   }
